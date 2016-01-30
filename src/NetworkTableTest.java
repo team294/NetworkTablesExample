e6c1d7c0ba0a6e2bdb07a4160f93c3dc2dd3fbaa
team294 <team294@bcr.org>
2016-01-29 20:59:03 -0800

    Initial version, by Don Sawdai

---
+++ src/NetworkTableTest.java
@@ -0,0 +1,53 @@
+
+import java.util.*;
+
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+public class NetworkTableTest {
+	
+/* Before running this program:
+	1. Run GRIP, including a "Publish LinesReport" that has Name "myLinesReport"
+	2. Start menu -> run "CMD", then type "ipconfig" at the prompt.  Find "IPv4 Address" 
+	     and put it in the setIPAddress command below.
+	3. Run C:\Users\team\wpilib\tools\OutlineViewer.jar.  Input your IP address and 
+	     click "Start Server".
+*/	
+	public static void main(String[] args) {
+
+		Set<String> keys;
+		double[] angles, angleDefault;
+		int i;
+		NetworkTable table;
+	
+		NetworkTable.setClientMode();
+		NetworkTable.setIPAddress("192.168.1.64");
+		table = NetworkTable.getTable("GRIP/myLinesReport");
+
+		myPrint(table.toString());
+
+		angleDefault = new double[] { -1.0 };
+		
+		for (i=0;i<3;i++) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			keys = table.getKeys();
+			
+			myPrint("Number of keys = " + keys.size());
+			myPrint("Keys = " + keys);
+						
+			angles = table.getNumberArray("angle", angleDefault );
+			
+			myPrint("angle array length = " + angles.length);
+			myPrint("angle[0] value = " + angles[0]);
+		}
+	}
+	
+	public static void myPrint(String s) {
+		System.out.println(s);
+	}
+}
